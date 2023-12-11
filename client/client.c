f5d5e5b9b21e46840862b2be32af3e611b961cb8
Cesar Guerrero <cguerrero@Cesars-MBP.attlocal.net>
2023-12-11 15:17:09 -0800

    This is the start of our program and both the server and client compile successfully. All future changes will be in separate client and server branches

---
+++ client/client.c
@@ -0,0 +1,11 @@
+/**
+ * Cesar Guerrero
+ * 12/11/23
+ * 
+ * @file This file handles client attempts to connect to the TCP Server Socket
+*/
+
+
+int main(){
+    return 0;
+}
---
+++ server/server.c
@@ -0,0 +1,11 @@
+/**
+ * Cesar Guerrero
+ * 12/11/23
+ * 
+ * @file This file handles starting the server and spawning new threads to
+ * handle each new client connection
+*/
+
+int main(){
+    return 0;
+}
