e6fa93f76653be9e960e2325d90f7f8e0afcc05c
Cesar Guerrero <cguerrero@Cesars-MBP.home>
2023-12-18 15:15:01 -0800

    STOP command working and tested

---
+++ client/tests/main.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#Cesar Guerrero
+#12/17/23
+
+#This is our MAIN CLIENT shell script. It is used to setup our test environment
+#and this script is in charge of running each individual test script
+
+#To run this script:
+# 1. Give it permission (chmod +x main.sh)
+# 2. Start the server
+# 3. Run the script
+
+#Clean up our executable
+make clean
+make
+
+mkdir -p ./tests/test-results
+
+rm -f ./tests/test-results/client-test.txt
+touch ./tests/test-results/client-test.txt
+
+#Test 1
+echo "TEST 1" >> ./tests/test-results/client-test.txt
+echo "Test Description: Ensure we get a usage error if no command is provided" >> ./tests/test-results/client-test.txt
+echo "EXPECTED OUTPUT:" >> ./tests/test-results/client-test.txt
+echo "'USAGE ERROR: ./client <string: WRITE | string: GET | string: RM | string: LS | string: STOP>'" >> ./tests/test-results/client-test.txt
+echo -e "\nACTUAL OUTPUT:" >> ./tests/test-results/client-test.txt
+./client >> ./tests/test-results/client-test.txt
+echo "Expect 2: $?" >> ./tests/test-results/client-test.txt
+echo "---" >> ./tests/test-results/client-test.txt
+
+#Test 2
+echo -e "\nTEST 2" >> ./tests/test-results/client-test.txt
+echo "Test Description: Ensure we get an error if an invalid command is provided" >> ./tests/test-results/client-test.txt
+echo "EXPECTED OUTPUT:" >> ./tests/test-results/client-test.txt
+echo "'ERROR: INVALID COMMAND - [nonsense]'" >> ./tests/test-results/client-test.txt
+echo -e "\nACTUAL OUTPUT:" >> ./tests/test-results/client-test.txt
+./client nonsense >> ./tests/test-results/client-test.txt
+echo "Expect 5: $?" >> ./tests/test-results/client-test.txt
+echo "---" >> ./tests/test-results/client-test.txt
+
+#To keep the code clean and concise, we wrote method specific tests into their own scripts
+./tests/stop.sh
---
+++ client/tests/stop.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#Cesar Guerrero
+#12/18/23
+
+#This is our STOP shell script. It is used to test the STOP command
+#It is called by main.sh
+
+#Clean up our executable
+make clean
+make
+
+#Clean up our tests output file
+rm -f ./tests/test-results/stop-test.txt
+touch ./tests/test-results/stop-test.txt
+
+#Test 1
+echo "TEST 1" >> ./tests/test-results/stop-test.txt
+echo "Test Description: Calling our STOP command should instruct the server to stop" >> ./tests/test-results/stop-test.txt
+echo "EXPECTED OUTPUT:" >> ./tests/test-results/stop-test.txt
+echo "'COMMAND: [STOP]'" >> ./tests/test-results/stop-test.txt
+echo "'SERVER RESPONSE: OK'" >> ./tests/test-results/stop-test.txt
+echo -e "\nACTUAL OUTPUT:" >> ./tests/test-results/stop-test.txt
+./client STOP >> ./tests/test-results/stop-test.txt
+echo "Expect 0: $?" >> ./tests/test-results/stop-test.txt
+echo "---" >> ./tests/test-results/stop-test.txt
+
+#Test 2
+echo -e "\nTEST 2" >> ./tests/test-results/stop-test.txt
+echo "Test Description: Concurrent calls should lead to a server response indicating that if the server is beginning a shutdown, new connections should be refused" >> ./tests/test-results/stop-test.txt
+echo "EXPECTED OUTPUT:" >> ./tests/test-results/stop-test.txt
+echo "'COMMAND: [STOP]'" >> ./tests/test-results/stop-test.txt
+echo "'SERVER RESPONSE: SHUT DOWN IN PROGRESS - CONNECTION REFUSED'" >> ./tests/test-results/stop-test.txt
+echo "'ERROR: Server failed to [STOP]'" >> ./tests/test-results/stop-test.txt
+echo -e "\nACTUAL OUTPUT:" >> ./tests/test-results/stop-test.txt
+./client STOP >> ./tests/test-results/stop-test.txt
+echo "Expect 10: $?" >> ./tests/test-results/stop-test.txt
+echo "---" >> ./tests/test-results/stop-test.txt
+
+
+#For Test 3 we are inputting a sleep() to ensure the server is fully shut down
+sleep 10
+
+#Test 3
+echo -e "\nTEST 3" >> ./tests/test-results/stop-test.txt
+echo "Test Description: Attempting to call a server that is stopped leads to an error" >> ./tests/test-results/stop-test.txt
+echo "EXPECTED OUTPUT:" >> ./tests/test-results/stop-test.txt
+echo "'COMMAND: [STOP]'" >> ./tests/test-results/stop-test.txt
+echo "'ERROR: Cannot connect to server'" >> ./tests/test-results/stop-test.txt
+echo -e "\nACTUAL OUTPUT:" >> ./tests/test-results/stop-test.txt
+./client STOP >> ./tests/test-results/stop-test.txt
+echo "Expect 4: $?" >> ./tests/test-results/stop-test.txt
+echo "---" >> ./tests/test-results/stop-test.txt
